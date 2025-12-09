c5205e431fc36ac7c806db9bb77d147d16843368
juan_ <alafresh16@gmail.com>
2025-12-08 19:48:30 -0500

    feat(app): creacion de primeros componentes y layout primeros componentes

---
+++ demo/src/app/components/shared/divider-content/divider-content.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-divider-content',
+  imports: [],
+  templateUrl: './divider-content.html',
+})
+export class DividerContent {}
---
+++ demo/src/app/components/shared/header-component/header-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-component',
+  imports: [],
+  templateUrl: './header-component.html',
+})
+export class HeaderComponent {}
